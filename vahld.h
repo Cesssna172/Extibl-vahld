502e7e2c31169348d075013918f6ff5e9bb84d84
Cesssna172 <168644058+Cesssna172@users.noreply.github.com>
2024-12-10 18:07:15 +0300

    Create vahld.h
    header file

---
+++ vahld.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+#ifndef Extibl_h
+#define Extibl_h
+
+class Extibl {
+public:
+	int size = 0;
+
+	void output() const;								//regular output
+	void ext(int amount = 1);							//func itself
+		int* arr{};
+		int* copy(int* arr1, int* arr2, int size);		// эти функции преимущественно технические 
+		void create(int length = 0);					// и преимущественно используются в коде функций
+										
+	void append(int num);								//добавляет в конец массива значение
+	void extend(int length_nns,int* new_nums);			//увеличивает значение на массив значений добавляя их в конец
+	void insert(int position, int number);				//вставляет число в середину массива, сдвигая все значения
+	void pop(int index);								//удаляет число по указанному индексу
+};
+#endif
